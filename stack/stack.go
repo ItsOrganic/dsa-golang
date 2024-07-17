a91d93cee11cf0df795f22dd0bf0f8f1c52811a0
ItsOrganic <stutyashivam@gmail.com>
2024-07-18 02:00:26 +0530

    basic changes and minor optimisations in stack

--- stack/stack.go
+++ stack/stack.go
@@ -8,6 +8,19 @@ type Stack struct {
 	items []int
 }
 
+func main() {
+	stack := &Stack{}
+	stack.Push(5)
+	stack.Push(6)
+	stack.PrintStack()
+	//stack.Pop()
+	fmt.Println("After popping one element:", stack)
+	fmt.Println("On Peeking:", stack.Peek())
+	fmt.Println("On Reversing the values present in the stack:", stack.Reverse())
+	fmt.Println("Check is the stack empty:", stack.isEmpty())
+	stack.PrintStack()
+}
+
 func (s *Stack) Push(item int) {
 	s.items = append(s.items, item)
 }
@@ -29,14 +42,12 @@ func (s *Stack) Reverse() []int {
 	}
 	return s.items
 }
-
-func main() {
-	stack := &Stack{}
-	stack.Push(5)
-	stack.Push(6)
-	fmt.Println("Stack :", stack)
-	//stack.Pop()
-	fmt.Println("After popping one element:", stack)
-	fmt.Println("On Peeking:", stack.Peek())
-	fmt.Println("On Reversing the values present in the stack:", stack.Reverse())
+func (s *Stack) isEmpty() bool {
+	if len(s.items) == 0 {
+		return true
+	}
+	return false
+}
+func (s *Stack) PrintStack() {
+	fmt.Println("The stack is :", s.items)
 }
